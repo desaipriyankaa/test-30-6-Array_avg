60f4d11cb3e56c989610be71b0ce26d6e6de4ab9
desaipriyankaa <priyanka.desai@klingelnberg.com>
2021-06-30 15:47:49 +0530

    Add project files.

---
+++ ArrayPractice2/Calculation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayPractice2
+{
+    class Calculation
+    {
+        // Taking input from user
+        public int[] UserInput()
+        {
+            Console.WriteLine("Input 5 numbers :");
+            int[] array = new int[5];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            return array;
+        }
+
+        // Printing values on output
+        public void PrintArray(int[] array)
+        {
+            Console.Write("[");
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("]");
+        }
+
+        // Calculating average
+        public void Average(int[] array)
+        {
+            int avg = 0;
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            avg = sum / 5;
+            Console.WriteLine(avg);
+        }
+    }
+}
---
+++ ArrayPractice2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ArrayPractice2
+{
+    //// Program to find the average of n numbers using arrays
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation calc = new Calculation();
+
+            //prints array
+            int[] arr = calc.UserInput();
+            calc.PrintArray(arr);
+
+            //sort array list
+            Array.Sort(arr);
+            calc.PrintArray(arr);
+
+            //print average
+            calc.Average(arr);
+        }
+    }
+}
